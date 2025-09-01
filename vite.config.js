667c9e52c662f27d62baf03c0d49a337c0f97584
EtoroAbasi <etoroabasiudoh88@gmail.com>
2025-09-01 07:45:57 +0100

    pulsePay

--- vite.config.js
+++ vite.config.js
@@ -43,6 +43,9 @@ export default defineConfig({
       qs: "qs",
       punycode: "punycode/",
       url: "url/",
+      "string_decoder": "string_decoder/",
+      http: "stream-http", 
+      os: "os-browserify/browser", 
       "rollup-plugin-node-polyfills/polyfills/util": "util/",
       "rollup-plugin-node-polyfills/polyfills/events": "events/",
       "rollup-plugin-node-polyfills/polyfills/stream": "stream-browserify",
@@ -50,7 +53,21 @@ export default defineConfig({
       "rollup-plugin-node-polyfills/polyfills/qs": "qs",
       "rollup-plugin-node-polyfills/polyfills/punycode": "punycode/",
       "rollup-plugin-node-polyfills/polyfills/url": "url/",
-      
+      "rollup-plugin-node-polyfills/polyfills/string-decoder": "string_decoder/",
+      "rollup-plugin-node-polyfills/polyfills/http": "stream-http",
+      "rollup-plugin-node-polyfills/polyfills/os": "os-browserify/browser",
+      "rollup-plugin-node-polyfills/polyfills/readable-stream/duplex": "readable-stream/duplex",
+      "rollup-plugin-node-polyfills/polyfills/readable-stream/readable": "readable-stream/readable",
+      "rollup-plugin-node-polyfills/polyfills/readable-stream/writable": "readable-stream/writable",
+      "rollup-plugin-node-polyfills/polyfills/readable-stream/transform": "readable-stream/transform",
+      "rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough":
+      "/src/polyfills/readable-stream/passthrough.js",
+      "rollup-plugin-node-polyfills/polyfills/timers":
+      "/src/polyfills/timers.js",
+      "rollup-plugin-node-polyfills/polyfills/vm":
+      "/src/polyfills/vm.js",
+      "react-router/dom": "react-router-dom",
+      "./postinstall.mjs": "/src/polyfills/postinstall.mjs",
     },
   },
 });
